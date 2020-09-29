edafeceadbd3c6514a074f6ee219dc9b87a0082e
Greg Bernstein <gregb@grotto-networking.com>
2020-09-29 12:08:03 -0700

    Added documentation.

--- reactAndD3.js
+++ reactAndD3.js
@@ -5,9 +5,12 @@ import SineWavePlot from "./reactComponents/sineWavePlot";
 
 let contents = <main>
     <h1>React and D3 Examples</h1>
+    <h2>Sine Wave Plot</h2>
+    <p>React does all the SVG drawing here, D3 computes the scales, and
+        the basic SVG path data string.
+    </p>
+    <SineWavePlot />
     <h2>Drawing SVG in React</h2>
     <SVGdrawReact />
-    <h2>Sine Wave Plot (To be written)</h2>
-    <SineWavePlot />
 </main>;
 ReactDOM.render(contents, document.getElementById("root"));
